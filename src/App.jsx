6b2f78b60af00068a86abf55a96199c7ffd807cd
rajesh151 <paliwal.rajesh.151@gmail.com>
2024-10-04 12:06:30 +0530

    feat: Added Text using props.children

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import CoreConcepts from "./components/CoreConcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 
-
 function App() {
   return (
     <div>
@@ -23,11 +22,10 @@ function App() {
           </ul>
         </section>
         <section id="examples">
-          <h2>Examples</h2> 
+          <h2>Examples</h2>
           <menu>
-
+            <TabButton>Components</TabButton>
           </menu>
-
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,3 +1,3 @@
-export default function TabButton() {
-    
+export default function TabButton(props) {
+    return <li><button>{props.children}</button></li>;
 }
